ee8f7e8a22a94d3f78d7a04d6ac95e8ceedfa167
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-07-29 20:16:19 +0530

    Add array solutions for even numbers and reversing array

--- arrays/03_allEvenNum.js
+++ arrays/03_allEvenNum.js
@@ -1,1 +1,4 @@
-const arr = [110, 50, 20, 180, 100];
+const arr=[1,2,3,4,5,6];
+const even=[];
+for(let n of arr){if(n%2===0) even.push(n);}
+console.log(even);
---
+++ arrays/04_ReverseArray.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5, 6];
+const reverse = arr.reverse();
+console.log(reverse) 
+console.log(arr) 
+
+const array = [10, 20, 30];
+const reversed = arr.toReversed();
+
+console.log(reversed); 
+console.log(array);  
